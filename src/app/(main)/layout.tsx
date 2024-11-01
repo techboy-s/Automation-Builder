c8cd223ee1e12bf373d2130755e25922b84cf253
techboy-s <shubhamkumarphp@gmail.com>
2024-11-01 16:09:12 +0530

    initial commit

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "../components/sidebar";
+
+type Props = { children: React.ReactNode };
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <Sidebar />
+      <div className="w-full">{props.children}</div>
+    </div>
+  );
+};
+
+export default Layout;
